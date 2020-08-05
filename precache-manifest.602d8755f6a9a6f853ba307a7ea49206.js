3e72aa653f75e878273f4ab3a9828e06cb8260e4
Barel Musenga <barelmusenga@barels-imac.home>
2020-08-05 01:41:11 -0400

    Updates

---
+++ precache-manifest.602d8755f6a9a6f853ba307a7ea49206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f8a0aba9b3575719cc51688de17c05",
+    "url": "/react-native-htmltags/index.html"
+  },
+  {
+    "revision": "4659706a46d0e44aa5d2",
+    "url": "/react-native-htmltags/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "aced35a66a63beae0a44",
+    "url": "/react-native-htmltags/static/css/main.a4e22caa.chunk.css"
+  },
+  {
+    "revision": "4659706a46d0e44aa5d2",
+    "url": "/react-native-htmltags/static/js/2.41ccbfd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-native-htmltags/static/js/2.41ccbfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aced35a66a63beae0a44",
+    "url": "/react-native-htmltags/static/js/main.9b0d7115.chunk.js"
+  },
+  {
+    "revision": "63846434e310683767ca",
+    "url": "/react-native-htmltags/static/js/runtime-main.2b6e6922.js"
+  }
+]);
